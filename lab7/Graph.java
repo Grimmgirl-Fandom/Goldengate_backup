fe13d52f2d48ba3076b794dde05929fcf5095e0d
Grace Swenson Hollis <130778996+Grimmgirl-Fandom@users.noreply.github.com>
2025-05-20 17:15:37 -0400

    Create Graph.java

---
+++ lab7/Graph.java
@@ -0,0 +1,15 @@
+
+
+public class Graph{
+
+    //node = state;
+    //we will have 51 nodes
+
+  //adjacency list
+        //this should be easy. 
+
+     public static void main(String[] args) 
+   {
+    UnitedStates us = new UnitedStates();
+   }
+}
